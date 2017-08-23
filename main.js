179ca6483d6be2a1804fc0cfcf8c62c209641d77
holmanhub <holman.eric85@gmail.com>
2017-08-22 20:25:09 -0500

    added JSON file

---
+++ main.js
@@ -0,0 +1,25 @@
+var cats = [
+	{name: "Fluffy", color: "green", url:"http://newsonia.com/media/upload/images/2014/09/19/SQK2tNq2QR3xnyPvE1DWllJcvFZmbVBn.jpg"},
+	{name: "Duke", color: "black", url:"https://iheartcats.com/wp-content/uploads/2015/11/21542389884_4f32d956db_z.jpg"},
+	{name: "Savage", color: "white", url:"https://kittybloger.files.wordpress.com/2013/08/white-cats-are-beautiful-30-photos-30.jpg"},
+];
+
+function domString(cats){
+	var catString = '';
+for (var i=0; i<cats.length; i++){
+		catString += `<div class="catBox">`;
+		catString += `<div class="catName">${cats[i].name}</div>`;
+		catString += `<div class="catColor">${cats[i].color}</div>`;
+		catString += `<img class="catImage" src="${cats[i].url}">`;
+		catString += `</div>`;
+	}
+	writeToDom(catString);
+
+}
+var catHolder = document.getElementById('kat-koral');
+
+function writeToDom(strang){
+	catHolder.innerHTML = strang;
+}
+
+domString(cats);
