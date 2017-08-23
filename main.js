a9580690eb37a4624b88993274b8ca96b7024502
holmanhub <holman.eric85@gmail.com>
2017-08-23 17:15:09 -0500

    added more JSON practices

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-var cats = [
-	{name: "Fluffy", color: "green", url:"http://newsonia.com/media/upload/images/2014/09/19/SQK2tNq2QR3xnyPvE1DWllJcvFZmbVBn.jpg"},
-	{name: "Duke", color: "black", url:"https://iheartcats.com/wp-content/uploads/2015/11/21542389884_4f32d956db_z.jpg"},
-	{name: "Savage", color: "white", url:"https://kittybloger.files.wordpress.com/2013/08/white-cats-are-beautiful-30-photos-30.jpg"},
-];
+// var cats = [
+// 	{name: "Fluffy", color: "green", url:"http://newsonia.com/media/upload/images/2014/09/19/SQK2tNq2QR3xnyPvE1DWllJcvFZmbVBn.jpg"},
+// 	{name: "Duke", color: "black", url:"https://iheartcats.com/wp-content/uploads/2015/11/21542389884_4f32d956db_z.jpg"},
+// 	{name: "Savage", color: "white", url:"https://kittybloger.files.wordpress.com/2013/08/white-cats-are-beautiful-30-photos-30.jpg"},
+// ];
 
 function domString(cats){
 	var catString = '';
 for (var i=0; i<cats.length; i++){
 		catString += `<div class="catBox">`;
 		catString += `<div class="catName">${cats[i].name}</div>`;
-		catString += `<div class="catColor">${cats[i].color}</div>`;
+		//catString += `<div class="catColor">${cats[i].color}</div>`;
 		catString += `<img class="catImage" src="${cats[i].url}">`;
 		catString += `</div>`;
 	}
@@ -22,4 +22,47 @@ function writeToDom(strang){
 	catHolder.innerHTML = strang;
 }
 
-domString(cats);
+//domString(cats);
+
+function executeThisCodeAfterFileLoads(){
+	console.log("this", this.responseText);
+	var data = JSON.parse(this.responseText);
+	domString(data.cats);
+}
+
+function executeThisCodeAfterFileErrors(){
+	console.log("Shit Broke");
+}
+
+var myRequest = new XMLHttpRequest();
+myRequest.addEventListener('load', executeThisCodeAfterFileLoads);
+myRequest.addEventListener('error', executeThisCodeAfterFileErrors);
+myRequest.open('GET', 'cats.json');
+myRequest.send();
+
+var myRequest2 = new XMLHttpRequest();
+myRequest.addEventListener('load', executeThisCodeAfterFileLoads);
+myRequest.addEventListener('error', executeThisCodeAfterFileErrors);
+myRequest.open('GET', 'dogs.json');
+myRequest.send();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
